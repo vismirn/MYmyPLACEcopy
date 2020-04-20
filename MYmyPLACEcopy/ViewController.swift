f3ea71f447b4ef43073f2341f2f63a87e0071fa5
Viktor <smirn04@yandex.ru>
2020-04-20 16:55:10 +0300

    Initial Commit

---
+++ MYmyPLACEcopy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MYmyPLACEcopy
+//
+//  Created by Viktor Smirnov on 20.04.2020.
+//  Copyright © 2020 Viktor Smirnov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
